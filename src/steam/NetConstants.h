94709ca007048e5d907d78fcaa0401e4e9222091
rutra8002 <arturkummer08@gmail.com>
2025-08-21 09:21:21 +0200

    goofy ahh comment

--- src/steam/NetConstants.h
+++ src/steam/NetConstants.h
@@ -1,7 +1,6 @@
 #pragma once
 
 // Unique lobby verification tag to distinguish this game's lobbies from other Spacewar (appid 480) lobbies.
-// Change this to any unique string for your project. Keep both key and value stable across builds.
 namespace NetConstants {
     inline constexpr const char* LOBBY_SIGN_KEY = "app_tag"; // metadata key
     inline constexpr const char* LOBBY_SIGN_VALUE = "ruterek"; // unique value
